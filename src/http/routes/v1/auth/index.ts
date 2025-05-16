8aa6bdf056da757e37a1dbd366c55a1989cda185
Vicentesan <vikom.sanchez@gmail.com>
2025-05-16 13:45:25 -0300

    chore: register route

--- src/http/routes/v1/auth/index.ts
+++ src/http/routes/v1/auth/index.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify'
 
 import { authenticateWithEmailAndPasswordRoute } from './authenticate-with-email-and-password-route'
+import { setup2FARoute } from './setup-2fa'
 
 export async function authRoutes(app: FastifyInstance) {
   app.register(authenticateWithEmailAndPasswordRoute)
+  app.register(setup2FARoute)
 }
